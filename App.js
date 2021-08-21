0fcca45431934228a4ece63211592c5226acf530
Ibrahim <ibrahimfayyaz26@gmail.com>
2021-08-22 00:16:06 +0500

    c

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Navigator from "./navigation/AppNavigator";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
-  return <Navigator />;
+  return (
+    <NativeBaseProvider>
+      <Navigator />
+    </NativeBaseProvider>
+  );
 }
